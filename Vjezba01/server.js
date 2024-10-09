292fabbc6384a2578ac5fab11d184e1e116910b5
nhuzjak20 <huzjakn5@gmail.com>
2024-10-10 01:56:47 +0200

    Uređen Obrzac i Dodana Dinamičnost

--- Vjezba01/server.js
+++ Vjezba01/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
+server.use(express.static(__dirname));
 
-server.use("/", (req, res) => {
-  res.redirect("/prijava");
+server.get("/prijava", (req, res) => {
+  console.log("Doslo je do prijave");
+  res.sendFile(__dirname + "/prijava.html");
 });
 
-server.get("/prijava", (req, res) => {});
-
-server.listen(5000, () => {
-  console.log(`Server pokrenut na portu: ${5000}`);
+server.listen(6500, () => {
+  console.log(`Server pokrenut na portu: ${6500}`);
 });
